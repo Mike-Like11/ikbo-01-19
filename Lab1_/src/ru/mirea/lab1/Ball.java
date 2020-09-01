a49550c5c97358ef200ec7b24775ccbc3cc46953
VishLi777 <VishLi777@github.com>
2020-09-01 11:05:08 +0300

    Lab1

---
+++ Lab1_/src/ru/mirea/lab1/Ball.java
@@ -0,0 +1,12 @@
+package ru.mirea.lab1;
+
+public class Ball {
+    int diameter;
+    String color;
+
+    void displayInfo(){
+        System.out.println("Diameter: " + diameter);
+        System.out.println("Color: " + color);
+    }
+}
+
